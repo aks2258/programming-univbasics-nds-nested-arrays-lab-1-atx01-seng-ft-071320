afe017b6eea7566ff46cb0851796064085faf8da
Arnie Serrano <arniekidd@gmail.com>
2020-06-25 18:01:42 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -23,7 +23,7 @@ def assembled_matrix
   # Make sure conventional produce is first, on the 'zeroth' / 'bottom' shelf
   
   storage=[
-    ORGANIC_PRODUCE
+    ORGANIC_PRODUCE,
     CONVENTIONAL_PRODUCE
     ]
   
